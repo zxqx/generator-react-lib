567f1523161d5b71968296fbbcde321c242f5b16
Zak Angelle <zaknotzak@gmail.com>
2016-10-23 01:19:44 -0500

    Clean up generator function

--- app/index.js
+++ app/index.js
@@ -1,19 +1,15 @@
-'use strict';
-
-var yeoman = require('yeoman-generator');
-var _s = require('underscore.string');
+const yeoman = require('yeoman-generator');
+const _s = require('underscore.string');
 
 module.exports = yeoman.generators.Base.extend({
   init: function() {
-    var cb = this.async();
+    const cb = this.async();
 
     this.prompt([
       {
         name: 'libraryName',
         message: 'What\'s the name of your library?',
-        filter: function(val) {
-          return _s.slugify(val);
-        }
+        filter: val => _s.slugify(val)
       },
       {
         name: 'libraryDescription',
@@ -21,23 +17,21 @@ module.exports = yeoman.generators.Base.extend({
       }
     ],
     function(props) {
-      var asyncCount = 0;
-      this.libraryName = props.libraryName.replace(/\s/g, '-').replace(/^react-/, '');
-      this.libraryNamePascalized = _s.capitalize(_s.camelize(this.libraryName));
+      this.libraryName = _s.slugify(props.libraryName.replace(/^react-/, ''));
       this.libraryNameCamelized = _s.camelize(this.libraryName);
+      this.libraryNamePascalized = _s.capitalize(this.libraryNameCamelized);
       this.libraryDescription = props.libraryDescription;
 
       this.template('gitignore', '.gitignore');
       this.template('eslintrc', '.eslintrc');
       this.template('webpack.config.js');
-      // needed so npm doesn't try to use it and fail
       this.template('_package.json', 'package.json');
       this.template('README.md');
       this.template('babelrc', '.babelrc');
-      this.directory('example', 'example');
-      this.directory('lib', 'lib');
-      this.directory('src', 'src');
-      this.directory('test', 'test');
+      this.directory('example');
+      this.directory('lib');
+      this.directory('src');
+      this.directory('test');
 
       cb();
 
