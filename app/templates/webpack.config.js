272b026cb783e01261a3f583a4ab36fab8c6b501
Scott Prue <scott@residenetwork.com>
2017-09-27 11:34:28 -0700

    No longer exporting commonjs or es version (styles can be affected by project webpack config). CSS loader used in webpack config (correctly exports styling).

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 const libraryName = '<%= libraryNameCamelized %>';
 const isProduction = process.env.NODE_ENV === 'production';
+
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const extractStyles = new ExtractTextPlugin({
+  filename: 'styles/[name].[contenthash].css',
+  allChunks: true,
+  // disable: !isProduction
+})
+const enableSourcemaps = true
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
@@ -23,7 +30,45 @@ module.exports = {
         test: /(\.jsx|\.js)$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: extractStyles.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: enableSourcemaps,
+                modules: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                minimize: {
+                  autoprefixer: {
+                    add: true,
+                    remove: true,
+                    browsers: ['last 2 versions'],
+                  },
+                  discardComments: {
+                    removeAll: true,
+                  },
+                  discardUnused: false,
+                  mergeIdents: false,
+                  reduceIdents: false,
+                  safe: true,
+                  sourcemap: enableSourcemaps,
+                }
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: enableSourcemaps
+              }
+            }
+          ]
+        })
+      },
     ]
   },
   plugins: isProduction ? [new UglifyJsPlugin({ minimize: true })] : []
