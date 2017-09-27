3552d979c4a86e0446a3f917bf30192fabc685c1
Scott Prue <scott@residenetwork.com>
2017-09-26 18:38:38 -0700

    Multiple module types (es, commonjs, and umd) as well as other build updates.
    * Webpack 3
    * babel-preset-env used in place of es-2015 and stage-0
    * lint:fix npm script added (`npm run lint:fix`)
    * watch npm script added (`npm run watch`)
    * clean npm script added (`npm run clean` used in new build command)

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -3,12 +3,13 @@ const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 const libraryName = '<%= libraryNameCamelized %>';
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    path: path.join(__dirname, 'lib'),
-    filename: libraryName + '.min.js',
+    path: path.join(__dirname, 'dist'),
+    filename: libraryName + (isProduction ? '.min.js' : '.js'),
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
@@ -22,13 +23,8 @@ module.exports = {
         test: /(\.jsx|\.js)$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        use: 'eslint-loader',
-        exclude: /node_modules/
       }
     ]
   },
-  plugins: [new UglifyJsPlugin({ minimize: true })]
+  plugins: isProduction ? [new UglifyJsPlugin({ minimize: true })] : []
 };
