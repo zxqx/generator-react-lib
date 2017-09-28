1d8d15cf9fed4d0fe402ff6d11b71572c54f4ac5
Scott Prue <scott@residenetwork.com>
2017-09-28 14:05:14 -0700

    Fixes made based on comments.
    webpack.config.js fixes:
      * ExtractTextPlugin is required
      * Build folder switched to lib over dist
      * extractStyles plugin added to plugins list

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 const libraryName = '<%= libraryNameCamelized %>';
 const isProduction = process.env.NODE_ENV === 'production';
+const enableSourcemaps = true
 
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const extractStyles = new ExtractTextPlugin({
   filename: 'styles/[name].[contenthash].css',
   allChunks: true,
-  // disable: !isProduction
+  disable: !isProduction
 })
-const enableSourcemaps = true
 
-module.exports = {
+const config = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'lib'),
     filename: libraryName + (isProduction ? '.min.js' : '.js'),
     library: libraryName,
     libraryTarget: 'umd',
@@ -71,5 +72,11 @@ module.exports = {
       },
     ]
   },
-  plugins: isProduction ? [new UglifyJsPlugin({ minimize: true })] : []
+  plugins: [extractStyles]
 };
+
+if (isProduction) {
+  config.plugins.push(new UglifyJsPlugin({ minimize: true }))
+}
+
+module.exports = config
