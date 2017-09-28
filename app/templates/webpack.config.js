89954a1b0763a7511281d85151818b7ceda39bc7
Scott Prue <scott@residenetwork.com>
2017-09-28 15:45:22 -0700

    Removed style-loader and added back es and commonjs versions
    Renamed FOLDER env var to BUILD_DIR

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -1,33 +1,38 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const libraryName = '<%= libraryNameCamelized %>';
 const isProduction = process.env.NODE_ENV === 'production';
-const enableSourcemaps = true
-const name = process.env.FOLDER ? libraryName : 'index';
-const filename = name + (isProduction ? '.min.js' : '.js');
-
-const extractStyles = new ExtractTextPlugin({
-  filename: 'styles/[name].[contenthash].css',
-  allChunks: true,
-  disable: !isProduction
-});
 
 const config = {
   entry: {
     main: path.join(__dirname, 'src/index.js')
   },
   output: {
-    path: path.join(__dirname, process.env.FOLDER || 'lib'),
-    filename,
+    path: path.join(__dirname, 'dist'),
+    filename: libraryName + (isProduction ? '.min.js' : '.js'),
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  externals: {
-    react: 'react'
-  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: './react',
+        commonjs: ['./react'],
+        amd: 'react',
+      },
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: './react-dom',
+        commonjs: ['./react-dom'],
+        amd: 'react-dom',
+      },
+    }
+  ],
   module: {
     rules: [
       {
@@ -35,47 +40,9 @@ const config = {
         use: 'babel-loader',
         exclude: /node_modules/
       },
-      {
-        test: /\.(sass|scss)$/,
-        loader: extractStyles.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: enableSourcemaps,
-                modules: true,
-                importLoaders: 2,
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-                minimize: {
-                  autoprefixer: {
-                    add: true,
-                    remove: true,
-                    browsers: ['last 2 versions'],
-                  },
-                  discardComments: {
-                    removeAll: true,
-                  },
-                  discardUnused: false,
-                  mergeIdents: false,
-                  reduceIdents: false,
-                  safe: true,
-                  sourcemap: enableSourcemaps,
-                }
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: enableSourcemaps
-              }
-            }
-          ]
-        })
-      },
     ]
   },
-  plugins: [extractStyles]
+  plugins: []
 };
 
 if (isProduction) {
